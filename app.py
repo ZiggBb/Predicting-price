55078cf248e1f30dc35a3103f71e484b55adb2bc
ZiggBb <110830642+ZiggBb@users.noreply.github.com>
2023-07-10 13:44:20 +0530

    yes

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pickle
+
+# Load the CSV file with the car data
+pred_df = pd.read_csv("./AB_US_2020.csv")
+
+# Write the title of the application on the UI
+st.write("""# Price Prediction App
+""")
+
+# Function to make the prediction using the pre-trained model
+def model_pred(reviews_per_month, latitude, longitude, id):
+
+    ## Load the pre-trained model using pickle
+    with open("db", "rb") as file:
+        reg_model = pickle.load(file)
+
+    # Prepare the input features
+    input_features = [[reviews, lat, long, id]]
+    return reg_model.predict(input_features)
+
+# Create two columns in the UI
+col1, col2, col3 = st.columns(3)
+
+row1, row2 = st.columns(2)
+
+# Create a textbox to put for the id
+id = row1.text_input("ID")
+
+# Create a slider to set the engine power
+latitude = row1.slider("Latitude",
+                        0, 50, step=1)
+
+# Create a slider to set the longitude
+longitude = row1.slider("Longitude",
+                        -200, 0, step=1)
+
+# Create a slider to set the reviews per month
+reviews = row1.slider("Reviews / Month",
+                        0, 10, step=1)
+
+
+
+
+
+# Create a button to trigger the prediction
+if(st.button("Predict Price")):
+
+    # Make the prediction
+    price = model_pred(reviews_per _month, latitude, longitude, id)
+
+     # Display the result on the UI
+    st.text("Predicted price: "+ str(price))
